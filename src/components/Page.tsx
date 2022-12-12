3190ac565da8ee20d79184fb77e489a203e803d7
José Hélio Veríssimoo Jr <joselio105@users.noreply.github.com>
2022-12-12 10:17:37 -0300

    fix(client): turn page properties not requireds

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,11 +4,11 @@ import Menu from "./Menu"
 
 interface Props {
     children: ReactNode
-    works: Work[]
-    slug: string
+    works?: Work[]
+    slug?: string
 }
 
-export default function Page( { children, works, slug }: Props ){
+export default function Page( { children, works=[], slug="" }: Props ){
     return (
         <div className="w-screen h-full min-h-screen">
             <div className="max-w-5xl mx-auto flex gap-4 items-start">
